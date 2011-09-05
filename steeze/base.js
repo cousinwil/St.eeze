a86ceb4d514ac99e34f1775c36ca3508bdd9ada5
Wil Everts <your@cousinwil.com>
2011-09-05 01:43:11 +0000

    version 0.3 of steeze - added webfonts, inset shadows, cleanup, sample inits, newest html5doctor reset css

--- steeze/base.js
+++ steeze/base.js
@@ -1,4 +1,4 @@
-/* Style + Ease = ST.EEZE.us Front End Starter-Kit v 0.20
+/* Style + Ease = ST.EEZE.us Front End Starter-Kit v 0.40
    Author: Wil Everts - http://cousinwil.com - @cousinwil
   
                                 (80 chars, the suggested maximum line length)-->
@@ -6,45 +6,70 @@
 
 var steeze = {
   
-    /* Google Analytics - allows us to connect st.eeze.track to your GA acct
-      ----------------------------------------------------------------------- */
-    google: {    
-      track: function(tracked){
-        console.log('Tracked: ' + tracked);
-        pageTracker._trackPageview(tracked);
-      }, // end st.google.track
-    }, // end st.google
+  /* Google 
+  
+    steeze.google.visit - allows us to connect to your Google Analytics acct and 
+                          track visits which occur w/o page load. Track things 
+                          like which button was clicked, etc using some url 
+                          hackery
+                         
+    steeze.google.font  - downloads webfonts from google 
+                         
+  --------------------------------------------------------------------------- */
+  google: {    
+    visit: function(tracked){
+      console.log('Tracked: ' + tracked);
+      pageTracker._trackPageview(tracked);
+    }, // end steeze.google.track
     
+    font: function() {
+      var wf = document.createElement('script');
+      wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+      wf.type = 'text/javascript';
+      wf.async = 'true';
+      var s = document.getElementsByTagName('script')[0];
+      s.parentNode.insertBefore(wf, s);
+    } // end steeze.google.font
+  }, // end steeze.google
   
   init: {
 
-  /* INITs for each controller or view or just as needed...
+  /* INITs for each controller, view, just as needed...
        These allow you to load up event handlers and functions specific to
        a template or view. 
        
        Each unique init, like my 'home' init, has a three init functions:
-           ready (on document ready), load (on load), and resize (on resize for redrawing 
-           absolute elements, etc.)
+       ready (on document ready), load (on load), and resize (on resize for 
+       redrawing absolute elements, etc.)
   --------------------------------------------------------------------------- */
   
     home: {
     
-      ready: function(){
-        //console.log('inside home ready init');
+      ready: function(info){
         
-      }, // end st.init.sample 
+        // see www.google.com/webfonts for fonts. I prefer to load these on load
+        // WebFontConfig = { google: { families: [ 'Open+Sans:latin' ] } }; 
+        // steeze.google.font();
+        console.log('steeze is inside steeze.init.home.ready. MSG: '+info);
+        
+      }, // end steeze.init.sample 
       
       load: function(){
-        //console.log('inside home load init');
+        
+        // see www.google.com/webfonts for fonts. I prefer to loading these here
+        WebFontConfig = { google: { families: [ 'Open+Sans:latin' ] } };
+        steeze.google.font();
+        console.log('steeze is inside steeze.init.load.ready. MSG: '+info);
           
-      }, // end st.init.load     
+      }, // end steeze.init.home.init.load     
       
-      resize: function(){
-        //console.log('inside home resize init');
+      size: function(){
+        console.log('steeze is inside steeze.init.size.ready. MSG: '+info);
           
-      } // end st.init.resize
+      } // end steeze.init.home.resize
     }
     
-  } // end st.init
+  } // end steeze.init
 
 };
