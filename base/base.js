3bd22c1b51c49fa6d9f018045a6eb60f807c9040
Wil Everts <your@cousinwil.com>
2011-09-14 00:08:51 -0700

    js cleanup - will add the google fns to the modules.js later

--- base/base.js
+++ base/base.js
@@ -6,24 +6,7 @@
 
 var base = {
   
-  /* Google 
-  
-    base.google.visit - allows us to connect to your Google Analytics acct and 
-                          track visits which occur w/o page load. Track things 
-                          like which button was clicked, etc using some url 
-                          hackery
-                         
-    base.google.font  - downloads webfonts from google 
-                         
-  --------------------------------------------------------------------------- */
-  google: {    
-    visit: function(tracked){
-      console.log('Tracked: ' + tracked);
-      pageTracker._trackPageview(tracked);
-    } // end base.google.track
-  }, // end base.google
-  
-  init: {
+  inits: {
 
   /* INITs for each controller, view, just as needed...
        These allow you to load up event handlers and functions specific to
@@ -37,16 +20,16 @@ var base = {
     main: {
     
       ready: function(info){
-        console.log('steeze is inside base.init.main.ready() MSG: '+info);
+        console.log('steeze is inside base.inits.main.ready() MSG: '+info);
       }, // end base.init.main.ready 
       load: function(info){
-        console.log('steeze is inside base.init.main.load() MSG: '+info);          
+        console.log('steeze is inside base.inits.main.load() MSG: '+info);          
       }, // end base.init.main.load     
       size: function(info){
-        console.log('steeze is inside base.init.main.size() MSG: '+info);
+        console.log('steeze is inside base.inits.main.size() MSG: '+info);
       }, // end base.init.main.size
       scroll: function(info){
-        console.log('steeze is inside base.init.main.scroll() MSG: '+info);
+        console.log('steeze is inside base.inits.main.scroll() MSG: '+info);
       } // end base.init.main.size
     
     }
